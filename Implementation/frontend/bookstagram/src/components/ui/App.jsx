17a035f3bf99c31e8b8a1afbe79b130a68b38b81
Khushi Bhatt <khushi.bhatt0405@gmail.com>
2026-02-09 00:00:13 -0800

    Navbar, Footer, Features, Input fields, testimonials, buttons - Created Landing page

--- Implementation/frontend/bookstagram/src/components/ui/App.jsx
+++ Implementation/frontend/bookstagram/src/components/ui/App.jsx
@@ -4,6 +4,9 @@ import ProtectedRoute from "./components/auth/ProtectedRoute";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import DashboardPage from "./pages/DashboardPage";
+import EditorPage from "./pages/EditorPage";
+import ViewBookPage from "./pages/ViewBookPage";
 import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
@@ -14,14 +17,26 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        
-        <Route 
+
+        {/* Protected Routes */}
+      <Route 
+        path="/dashboard" 
+        element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} 
+      />
+      <Route 
+        path="/editor/:bookId" 
+        element={<ProtectedRoute><EditorPage /></ProtectedRoute>} 
+      />
+      <Route 
+        path="/view-book/:bookId" 
+        element={<ProtectedRoute><ViewBookPage /></ProtectedRoute>} 
+      />
+      <Route 
         path="/profile" 
         element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} 
       />
-        
       </Routes>
-       </div>
+    </div>
   );
 };
 
