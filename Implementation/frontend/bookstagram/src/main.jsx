219e7d8cb79f40e14c1b3e72e276756f5715da8a
Aditi <aditigirdhar0044@gmail.com>
2026-03-03 13:29:26 -0800

    delete googleauth provider in main.jsx

--- Implementation/frontend/bookstagram/src/main.jsx
+++ Implementation/frontend/bookstagram/src/main.jsx
@@ -2,20 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <AuthProvider>
-        <BrowserRouter>
-          <App />
-          <Toaster position="bottom-right" />
-        </BrowserRouter>
-      </AuthProvider>
-    </GoogleOAuthProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+        <Toaster position="botton-right" />
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
+
