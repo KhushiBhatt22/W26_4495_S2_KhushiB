3e16f97abfbc19733f4fb5fdd7f650e32de29687
Aditi <aditigirdhar0044@gmail.com>
2026-03-04 22:36:40 -0800

    Add like.js in models

--- Implementation/backend/models/Like.js
+++ Implementation/backend/models/Like.js
@@ -6,17 +6,17 @@ const likeSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-    }, // person who liked
+    }, 
     bookId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Book",
       required: true,
-    }, // book that was liked
+    }, 
   },
   { timestamps: true }
 );
 
-// Prevent duplicate likes
+
 likeSchema.index({ userId: 1, bookId: 1 }, { unique: true });
 
 module.exports = mongoose.model("Like", likeSchema);
