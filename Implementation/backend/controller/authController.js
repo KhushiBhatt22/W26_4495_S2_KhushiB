2fa58aeac1b3471b49cf081eaf0c3bf20d7aec7b
Aditi <aditigirdhar0044@gmail.com>
2026-03-15 19:23:36 -0800

    change authRoute.js add new fuctions  and change path for upload photos in authcontoller

--- Implementation/backend/controller/authController.js
+++ Implementation/backend/controller/authController.js
@@ -148,7 +148,7 @@ exports.uploadProfilePhoto = async (req, res) => {
     if (!user) return res.status(404).json({ message: "User not found" });
     if (!req.file) return res.status(400).json({ message: "No file uploaded" });
 
-    const avatarPath = `/uploads/${req.file.filename}`;
+    const avatarPath = `/backend/uploads/${req.file.filename}`;
     user.avatar = avatarPath;
     await user.save();
 
