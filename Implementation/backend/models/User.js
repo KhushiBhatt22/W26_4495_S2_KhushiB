5b73e58f9716049976e5640745ef4f0d182b1688
Aditi <aditigirdhar0044@gmail.com>
2026-03-28 19:02:06 -0800

    remove age and gender field

--- Implementation/backend/models/User.js
+++ Implementation/backend/models/User.js
@@ -10,8 +10,6 @@ const userSchema = new mongoose.Schema(
     avatar: { type: String, default: "" },
     bio: { type: String, default: "", maxlength: 160 },
     isPro: { type: Boolean, default: false },
-    age: { type: Number, default: null },
-    gender: { type: String, enum: ["male", "female", "other"], default: null },
     isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }
