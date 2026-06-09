15560d38bc19d3e8d3c5043bee0622d1a5e39e4a
Khushi Bhatt <khushi.bhatt0405@gmail.com>
2026-06-09 13:20:59 -0700

    Updated the firebase authentication file

--- Implementation/frontend/bookstagram/src/firebase.js
+++ Implementation/frontend/bookstagram/src/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAXLFhsnpkQ9-0m1tHgZde1yrBAe7NSgC4",
-  authDomain: "bookstagram-27ca2.firebaseapp.com",
-  projectId: "bookstagram-27ca2",
-  storageBucket: "bookstagram-27ca2.firebasestorage.app",
-  messagingSenderId: "962462789288",
-  appId: "1:962462789288:web:66ab40e94570c599c55c9d",
-  measurementId: "G-MPWR00ELH6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
