4f224aec771942ebc2ef03ea37c9b99130c629f1
Khushi Bhatt <146828796+KhushiBhatt22@users.noreply.github.com>
2026-06-09 13:13:03 -0700

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- Implementation/frontend/bookstagram/src/firebase.js
+++ Implementation/frontend/bookstagram/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXLFhsnpkQ9-0m1tHgZde1yrBAe7NSgC4",
-  authDomain: "bookstagram-27ca2.firebaseapp.com",
-  projectId: "bookstagram-27ca2",
-  storageBucket: "bookstagram-27ca2.firebasestorage.app",
-  messagingSenderId: "962462789288",
-  appId: "1:962462789288:web:66ab40e94570c599c55c9d",
-  measurementId: "G-MPWR00ELH6"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
